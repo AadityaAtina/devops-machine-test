31817309950a7ddfce4f31c53ec9cf20b668940d
Aaditya Yemde <aadityay@atinatechnology.in>
2026-04-07 11:02:55 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,6 +9,12 @@ variable "location" {
   default     = "Central India"
 }
 
+variable "vnet_name" {
+  type        = string
+  description = "Virtual network name"
+  default     = "devops-test-vnet"
+}
+
 variable "acr_name" {
   type        = string
   description = "Azure Container Registry name (must be globally unique)"
@@ -29,4 +35,32 @@ variable "aks_vm_size" {
   type        = string
   description = "AKS node VM size"
   default     = "Standard_B2s"
+}
+
+variable "mysql_server_name" {
+  type        = string
+  description = "MySQL Flexible Server name (must be globally unique)"
+}
+
+variable "mysql_admin_user" {
+  type        = string
+  description = "MySQL administrator username"
+  default     = "adminuser"
+}
+
+variable "mysql_admin_password" {
+  type        = string
+  description = "MySQL administrator password - store in Key Vault, do NOT hardcode"
+  sensitive   = true
+}
+
+variable "mysql_db_name" {
+  type        = string
+  description = "MySQL database name"
+  default     = "appdb"
+}
+
+variable "key_vault_name" {
+  type        = string
+  description = "Azure Key Vault name (must be globally unique)"
 }
